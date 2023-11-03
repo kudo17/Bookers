0d70d89f1868f65883716e91a5de5750a1fb38ef
EC2 Default User <ec2-user@ip-172-31-7-67.ap-northeast-1.compute.internal>
2023-11-03 06:04:01 +0000

    [Finish] その他ページのレイアウトとリンクの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,18 +4,39 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to "/"
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
   
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+  
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
   end
   
   private
